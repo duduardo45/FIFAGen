e67098fcf24f20d0e4a21edc2922fe4f2e686583
BernardoLopes11 <25655795-BernardoLopes11@users.noreply.replit.com>
2023-11-14 16:45:13 +0000

    botei as paginas que fizemos antes

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,4 +20,9 @@ from FIFAGen import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('sobre', views.sobre, name='index'),
+  
+    path('criar_jogador', views.criar_jogador, name='criar
 ]
+
+    
